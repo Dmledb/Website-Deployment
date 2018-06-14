5d0341b2920ee04872198827e28f89bc34ed1818
Dylan Ledbetter <guestaccount@Dylans-MacBook-Pro.local>
2018-06-14 15:04:57 -0500

    standard push

--- project/settings.py
+++ project/settings.py
@@ -36,8 +36,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'personalapp',
-    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
